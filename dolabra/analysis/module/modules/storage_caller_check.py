49ab169f896c00053ee6759036ea8b5ba8da9db7
David Loz <lozdav3@gmail.com>
2023-04-02 22:54:47 +0200

    wip: refine the storage caller check, start of the function detection is wip

--- dolabra/analysis/module/modules/storage_caller_check.py
+++ dolabra/analysis/module/modules/storage_caller_check.py
@@ -2,115 +2,96 @@ import logging
 from typing import Optional
 from mythril.laser.ethereum.state.global_state import GlobalState
 from mythril.laser.smt.bitvec import BitVec
-from mythril.analysis.module.base import DetectionModule, EntryPoint
 
-from mythril.analysis.potential_issues import (
-    PotentialIssue,
-    get_potential_issues_annotation,
-)
+from dolabra.analysis.module.modules.basemodule import BaseModule
+
 
 log = logging.getLogger(__name__)
 
-class InputTaint:
-    """ Class to be used as annotation for tainted input elements. """
-
-    def __init__(self, taint_id: str):
-        self.taint_id = taint_id
+class CalldataTaint:
+    """ Class to be used as annotation for CALLDATALOAD elements. """
 
     def __hash__(self):
-        return hash((type(self), self.taint_id))
+        return hash(type(self))
 
-    def __eq__(self, other: 'InputTaint'):
-        return isinstance(other, InputTaint) and self.taint_id == other.taint_id
+    def __eq__(self, other: 'CalldataTaint'):
+        return isinstance(other, CalldataTaint)
+
+
+class StartFunctionTaint:
+    """ Class to be used as annotation for the start of the function. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'StartFunctionTaint'):
+        return isinstance(other, StartFunctionTaint)
+    
+class CallerTaint:
+    """ Class to be used as annotation for CALLER elements. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'CallerTaint'):
+        return isinstance(other, CallerTaint)
 
 
 class StorageTaint:
-    """ Class to be used as annotation for SLOAD elements with tainted input. """
+    """ Class to be used as annotation for SLOAD elements. """
 
-    def __init__(self, taint_id: str):
-        self.taint_id = taint_id
+    def __init__(self, storage_address: Optional[int] = None):
+        self.storage_address = storage_address
 
     def __hash__(self):
-        return hash((type(self), self.taint_id))
+        return hash((type(self), self.storage_address))
 
     def __eq__(self, other: 'StorageTaint'):
-        return isinstance(other, StorageTaint) and self.taint_id == other.taint_id
+        return self.storage_address == other.storage_address
 
 
-class StorageCallerCheck(DetectionModule):
-    name = "StorageCallerCheck"
-    description = "Detects when a contract checks if a tainted input is equal to a stored address before execution."
-    entry_point = EntryPoint.CALLBACK
+class StorageCallerCheck(BaseModule):
+    pattern_name = "StorageCallerCheck"
+    #description = "Detects when a contract checks if a tainted input is equal to a stored address before execution."
+
+    pre_hooks = ['JUMPI']
+    post_hooks = ['CALLER', 'SLOAD', 'CALLDATALOAD'] 
 
     def __init__(self):
-        self.pre_hooks = ['JUMPI']
-        self.post_hooks = ['CALLER', 'SLOAD', 'EQ']
+        self.is_start_of_function = False
+        self.essential_operations = set(['SSTORE', 'CALL', 'CALLCODE', 'DELEGATECALL', 'STATICCALL'])
         super().__init__()
 
-    def _execute(self, state: GlobalState) -> None:
-        potential_issues = self._analyze_state(state)
+    def _contains_both_taints(self, state: GlobalState) -> bool:
+        return (CallerTaint() in state.mstate.stack[-2].annotations and
+                 self._has_annotation(state.mstate.stack[-2], StorageTaint)) \
+                if state else False
+                               
+    def _analyze(self, state: GlobalState, prev_state: Optional[GlobalState] = None) -> Optional[dict]:
+        if prev_state and prev_state.instruction['opcode'] == 'CALLDATALOAD':
+            state.mstate.stack[-1].annotate(CalldataTaint())
 
-        annotation = get_potential_issues_annotation(state)
-        annotation.potential_issues.extend(potential_issues)    
+        if prev_state and prev_state.instruction['opcode'] == 'SLOAD' and prev_state.mstate.stack[-1].symbolic is False:
+            index = prev_state.mstate.stack[-1].value
+            if index <= 0xFF:
+                # Restrict memorizing storage keys that result from some sort of hashing
+                # by checking if the index is less than 256.
+                state.mstate.stack[-1].annotate(StorageTaint(index))
+        elif prev_state and prev_state.instruction['opcode'] == 'CALLER':
+            state.mstate.stack[-1].annotate(CallerTaint())
 
-    def _analyze_state(self, state: GlobalState, prev_state: Optional[GlobalState] = None) -> Optional[dict]:
-        if prev_state and prev_state.instruction['opcode'] == 'CALLER':
-            state.mstate.stack[-1].annotate(InputTaint('caller'))
-        elif prev_state and prev_state.instruction['opcode'] == 'SLOAD':
-            if self._has_annotation(prev_state.mstate.stack[-1], InputTaint):
-                taint_id = self._get_annotation(prev_state.mstate.stack[-1], InputTaint).taint_id
-                state.mstate.stack[-1].annotate(StorageTaint(taint_id))
-
-        log.info(
-                f"Analyzed {state.instruction['opcode']} at address {state.instruction['address']} in function ({state.environment.active_function_name})"
+        if state.instruction['opcode'] == 'JUMPI' and self._contains_both_taints(state):            
+            storage_address = self._retrieve_storage_address(state.mstate.stack[-2])
+            log.info(
+                f"Analyzed {state.instruction['opcode']} at address_index {storage_address} in function ({state.environment.active_function_name})"
             )
 
-        if state.instruction['opcode'] == 'EQ':
-            if (self._has_annotation(state.mstate.stack[-1], StorageTaint) and
-                    self._has_annotation(state.mstate.stack[-2], InputTaint)):
-                storage_taint = self._get_annotation(state.mstate.stack[-1], StorageTaint)
-                input_taint = self._get_annotation(state.mstate.stack[-2], InputTaint)
-                if storage_taint.taint_id != input_taint.taint_id:
-                    state.mstate.stack[-1].annotate(InputTaint(input_taint.taint_id))
-            elif (self._has_annotation(state.mstate.stack[-2], StorageTaint) and
-                self._has_annotation(state.mstate.stack[-1], InputTaint)):
-                storage_taint = self._get_annotation(state.mstate.stack[-2], StorageTaint)
-                input_taint = self._get_annotation(state.mstate.stack[-1], InputTaint)
-                if storage_taint.taint_id != input_taint.taint_id:
-                    state.mstate.stack[-1].annotate(InputTaint(storage_taint.taint_id))
-
-            if (self._has_annotation(state.mstate.stack[-1], InputTaint) and
-                self._has_annotation(state.mstate.stack[-2], StorageTaint)):
-
-                function_id = state.environment.active_function_name
-
-                log.info(
-                    f"Encountered {state.instruction['opcode']} at address {state.instruction['address']} in function ({function_id})"
-                )
-            
-                issue = PotentialIssue(
-                    contract=state.environment.active_account.contract_name,
-                    function_name=state.environment.active_function_name,
-                    address=state.get_current_instruction()['address'],
-                    swc_id="SWC-XXXX",
-                    title="Storage Caller Check",
-                    severity="Low",
-                    description="Detected a condition where a tainted input is checked against a stored address.",
-                    bytecode=state.environment.code.bytecode,
-                )
-                return [issue]
-        return []
-
-    def _has_annotation(self, element, annotation_class):
-        for annotation in element.annotations:
-            if isinstance(annotation, annotation_class):
-                return True
-        return False
+        return None
 
     def _retrieve_storage_address(self, bitvec: BitVec) -> Optional[int]:
         """ Helper function to retrieve the *storage_address* attribute from a BitVec instance. """
         for annotation in bitvec.annotations:
-            if isinstance(annotation, Storage):
+            if isinstance(annotation, StorageTaint):
                 return annotation.storage_address
         return None
 
