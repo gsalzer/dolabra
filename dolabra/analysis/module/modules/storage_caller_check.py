32b36f2e729deb3f567861d069758f0e844f232c
David Loz <lozdav3@gmail.com>
2023-04-17 23:06:39 +0200

    correct storage caller check logic

--- dolabra/analysis/module/modules/storage_caller_check.py
+++ dolabra/analysis/module/modules/storage_caller_check.py
@@ -1,98 +1,67 @@
 import logging
+import traceback
 from typing import Optional
 from mythril.laser.ethereum.state.global_state import GlobalState
 from mythril.laser.smt.bitvec import BitVec
 
 from dolabra.analysis.module.modules.basemodule import BaseModule
+from dolabra.analysis.module.modules.taints import (
+    PushFourTaint,
+    PushTwoTaint,
+    EqualTaint,
+    CallerTaint,
+    JumpiTaint,
+    StorageLoadTaint
+)
 
+log = logging.getLogger(__name__) 
 
-log = logging.getLogger(__name__)
-
-class CalldataTaint:
-    """ Class to be used as annotation for CALLDATALOAD elements. """
-
-    def __hash__(self):
-        return hash(type(self))
-
-    def __eq__(self, other: 'CalldataTaint'):
-        return isinstance(other, CalldataTaint)
-
-
-class StartFunctionTaint:
-    """ Class to be used as annotation for the start of the function. """
-
-    def __hash__(self):
-        return hash(type(self))
-
-    def __eq__(self, other: 'StartFunctionTaint'):
-        return isinstance(other, StartFunctionTaint)
-    
-class CallerTaint:
-    """ Class to be used as annotation for CALLER elements. """
-
-    def __hash__(self):
-        return hash(type(self))
-
-    def __eq__(self, other: 'CallerTaint'):
-        return isinstance(other, CallerTaint)
-
-
-class StorageTaint:
-    """ Class to be used as annotation for SLOAD elements. """
-
-    def __init__(self, storage_address: Optional[int] = None):
-        self.storage_address = storage_address
-
-    def __hash__(self):
-        return hash((type(self), self.storage_address))
-
-    def __eq__(self, other: 'StorageTaint'):
-        return self.storage_address == other.storage_address
-
+essential_operations = ['SSTORE', 'CALL', 'CALLCODE', 'DELEGATECALL', 'STATICCALL']
 
 class StorageCallerCheck(BaseModule):
     pattern_name = "StorageCallerCheck"
+    jumpdest_counter = 0
     #description = "Detects when a contract checks if a tainted input is equal to a stored address before execution."
 
     pre_hooks = ['JUMPI']
-    post_hooks = ['CALLER', 'SLOAD', 'CALLDATALOAD'] 
+    post_hooks = ['CALLER', 'SLOAD', 'PUSH4', 'EQ', 'PUSH2'] + essential_operations
 
     def __init__(self):
-        self.is_start_of_function = False
-        self.essential_operations = set(['SSTORE', 'CALL', 'CALLCODE', 'DELEGATECALL', 'STATICCALL'])
+        self.function_signatures = set()
+        self.functions_containing_auth = set()
         super().__init__()
-
-    def _contains_both_taints(self, state: GlobalState) -> bool:
-        return (CallerTaint() in state.mstate.stack[-2].annotations and
-                 self._has_annotation(state.mstate.stack[-2], StorageTaint)) \
-                if state else False
-                               
+    
+    def _annotate_function_start(self, state: GlobalState, prev_state: Optional[GlobalState] = None) -> bool:
+        if prev_state and prev_state.instruction['opcode'] == 'PUSH4':            
+            function_signature = state.mstate.stack[-1].value
+            self.function_signatures.add(function_signature)
+            state.mstate.stack[-1].annotate(PushFourTaint(function_signature))
+        elif prev_state and prev_state.instruction['opcode'] == 'EQ' and PushFourTaint(prev_state.mstate.stack[-1].value) in (state.mstate.stack[-1].annotations):
+            state.mstate.stack[-1].annotate(EqualTaint())   
+        elif prev_state and prev_state.instruction['opcode'] == 'PUSH2' and len(state.mstate.stack) > 1 and EqualTaint() in (state.mstate.stack[-2].annotations):
+            state.mstate.stack[-2].annotate(PushTwoTaint())
+        if state.instruction['opcode'] == 'JUMPI' and PushTwoTaint() in (state.mstate.stack[-2].annotations) :
+            state.mstate.stack[-2].annotate(JumpiTaint())
+    
     def _analyze(self, state: GlobalState, prev_state: Optional[GlobalState] = None) -> Optional[dict]:
-        if prev_state and prev_state.instruction['opcode'] == 'CALLDATALOAD':
-            state.mstate.stack[-1].annotate(CalldataTaint())
-
-        if prev_state and prev_state.instruction['opcode'] == 'SLOAD' and prev_state.mstate.stack[-1].symbolic is False:
-            index = prev_state.mstate.stack[-1].value
-            if index <= 0xFF:
-                # Restrict memorizing storage keys that result from some sort of hashing
-                # by checking if the index is less than 256.
-                state.mstate.stack[-1].annotate(StorageTaint(index))
-        elif prev_state and prev_state.instruction['opcode'] == 'CALLER':
-            state.mstate.stack[-1].annotate(CallerTaint())
-
-        if state.instruction['opcode'] == 'JUMPI' and self._contains_both_taints(state):            
-            storage_address = self._retrieve_storage_address(state.mstate.stack[-2])
-            log.info(
-                f"Analyzed {state.instruction['opcode']} at address_index {storage_address} in function ({state.environment.active_function_name})"
-            )
-
+        self._annotate_function_start(state, prev_state)
+        if prev_state and prev_state.instruction['opcode'] not in essential_operations:
+            if prev_state and prev_state.instruction['opcode'] == 'SLOAD' and prev_state.mstate.stack[-1].symbolic is False:
+                index = prev_state.mstate.stack[-1].value
+                if index <= 0xFF:
+                    # Restrict memorizing storage keys that result from some sort of hashing
+                    # by checking if the index is less than 256.
+                    state.mstate.stack[-1].annotate(StorageLoadTaint(index))
+            elif prev_state and prev_state.instruction['opcode'] == 'CALLER':
+                state.mstate.stack[-1].annotate(CallerTaint())
+            elif prev_state and prev_state.instruction['opcode'] == 'EQ' and \
+                (CallerTaint() in state.mstate.stack[-1].annotations and self._has_annotation(state.mstate.stack[-1], StorageLoadTaint)):
+                state.mstate.stack[-1].annotate(EqualTaint()) 
+                            
+            elif state.instruction['opcode'] == 'JUMPI' and EqualTaint() in state.mstate.stack[-2].annotations:
+                active_function = state.environment.active_function_name
+                if active_function not in self.functions_containing_auth:
+                    self.functions_containing_auth.add(active_function)
+                    return {'contract': state.environment.active_account.contract_name, 'pattern': self.pattern_name, 'function_name': active_function}
+            
         return None
-
-    def _retrieve_storage_address(self, bitvec: BitVec) -> Optional[int]:
-        """ Helper function to retrieve the *storage_address* attribute from a BitVec instance. """
-        for annotation in bitvec.annotations:
-            if isinstance(annotation, StorageTaint):
-                return annotation.storage_address
-        return None
-
-detector = StorageCallerCheck()
--- dolabra/analysis/module/modules/taints.py
+++ dolabra/analysis/module/modules/taints.py
@@ -9,6 +9,27 @@ class PushOneTaint:
     def __eq__(self, other: 'PushOneTaint'):
         return isinstance(other, PushOneTaint)        
     
+class PushTwoTaint:
+    """ Class to be used as annotation for PUSH2 elements. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'PushTwoTaint'):
+        return isinstance(other, PushTwoTaint) 
+    
+class PushFourTaint:
+    """ Class to be used as annotation for PUSH4 elements. """
+
+    def __init__(self, function_signature: Optional[int] = None):
+        self.function_signature = function_signature
+
+    def __hash__(self):
+        return hash(type(self))
+    
+    def __eq__(self, other: 'PushFourTaint'):
+        return isinstance(other, PushFourTaint) and self.function_signature == other.function_signature
+    
 class DupOneTaint:
     """ Class to be used as annotation for DUP1 elements. """
 
@@ -77,3 +98,39 @@ class StorageSaveTaint:
 
     def __eq__(self, other: 'StorageSaveTaint'):
         return isinstance(other, StorageSaveTaint) and self.storage_address == other.storage_address
+    
+class EqualTaint:
+    """ Class to be used as annotation for EQ elements. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'EqualTaint'):
+        return isinstance(other, EqualTaint)  
+    
+class CalldataTaint:
+    """ Class to be used as annotation for CALLDATALOAD elements. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'CalldataTaint'):
+        return isinstance(other, CalldataTaint)
+    
+class CallerTaint:
+    """ Class to be used as annotation for CALLER elements. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'CallerTaint'):
+        return isinstance(other, CallerTaint)
+    
+class JumpiTaint:
+    """ Class to be used as annotation for JUMPI elements. """
+
+    def __hash__(self):
+        return hash(type(self))
+
+    def __eq__(self, other: 'JumpiTaint'):
+        return isinstance(other, JumpiTaint)
