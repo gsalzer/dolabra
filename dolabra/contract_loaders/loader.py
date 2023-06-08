0f8e46a17a9e08f0ac2597d32cfd7bfa6c0623c0
Gernot Salzer <salzer@logic.at>
2023-06-08 22:48:14 +0200

    Loader for runtime code added

--- dolabra/contract_loaders/loader.py
+++ dolabra/contract_loaders/loader.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from dolabra.contract_loaders.contract_loader import ContractLoader
 from dolabra.contract_loaders.binary_loader import BinaryLoader
+from dolabra.contract_loaders.runtime_loader import RuntimeLoader
 from dolabra.contract_loaders.solidity_loader import SolidityLoader
 from dolabra.contract_loaders.jsonrpc_loader import JsonRpcLoader
 
@@ -9,11 +10,13 @@ class LoaderType(Enum):
     BINARY = 1
     SOLIDITY = 2
     JSON_RPC = 3
+    RUNTIME = 4
 
 class Loader():
     def get_contract(loader_type: LoaderType, **options) -> ContractLoader:
         switcher = {
             LoaderType.BINARY:   BinaryLoader.create,
+            LoaderType.RUNTIME:  RuntimeLoader.create,
             LoaderType.SOLIDITY: SolidityLoader.create,
             LoaderType.JSON_RPC: JsonRpcLoader.create
         }
