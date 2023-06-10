b2a122cacd9a2467cb17652ccc4421f0252573b7
David Loz <lozdav3@gmail.com>
2023-06-10 11:44:34 +0200

    correct benchmark

--- dolabra/benchmark/benchmark.py
+++ dolabra/benchmark/benchmark.py
@@ -225,8 +225,8 @@ def generate_contract_sample(instance_count: int,
                              sample_size: int,                             
                              ) -> Set[int]:    
     
-    #return set(random.sample(range(0, instance_count), sample_size))
-    return set(random.sample(range(0, 150), sample_size))
+    return set(random.sample(range(0, instance_count), sample_size))
+    #return set(random.sample(range(0, 150), sample_size))
 
 def count_files_in_directory(directory):
     return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
@@ -249,49 +249,61 @@ def new_benchmark(args) -> None:
     strategy_loader.set_modules([MODULES[strategy_name]()])
     positive_instances = set()
     files = glob.glob(os.path.join(args.dirpath, "*.hex"))
-    for i, file_name in enumerate(files):
-        #with open(args.filename, 'r') as csv_file:
-        with open(file_name, 'r') as file:
-            print("file_name", file_name)
-            base_file_name = os.path.basename(file_name)
-            # Remove the file extension
-            base_file_name_without_extension = os.path.splitext(base_file_name)[0]
-            block_id, target_address = base_file_name_without_extension.split('-')
-            #print("file content", file.read())
-            
-            #if i not in contract_sample:
-            #    continue
-            log.info('Analyzing contract %d/%d at address %s', i + 1, instance_count, target_address)
-            contract_loader = Loader.get_contract(LoaderType.BINARY, path=file_name)
-            print(contract_loader)
-            #loader_factory = get_factory(LoaderFactoryType.JSON_RPC, address=target_address, rpc=rpc)
-            #contract_loader = loader_factory.create()
-            symbolic_analysis = SymbolicWrapper(contract_loader)    
-            report = symbolic_analysis.run_analysis()
-            pprint.pprint(report, width=1)
-            #analysis_report = SymbolicWrapper().execute(contract_loader=contract_loader, timeout=args.timeout, max_depth=args.max_depth)
-            
-            if sum(len(report_item) for report_item in report) > 0:
-                positive_instances.add(i)
-            else:
-                log.info('Nothing found for contract %d/%d at address %s', i + 1, instance_count, target_address)
-            
-            detected_functions = [result.__dir__
-                                for report_item in report if len(report_item) > 0
-                                for result in report_item]
-            
-            #compiler_version = row[args.version_column] if args.version_column is not None else None
-            function_hashes = contract_loader.disassembly().func_hashes if contract_loader.disassembly() else []
+    #print(files)
 
-            print(detected_functions)
-            #benchmark_report.add_result(Result(function_hashes, target_address, i, detected_functions, compiler_version=compiler_version))
-            strategy_loader.reset_modules()
+    # Generate the contract sample indexes
+    sampled_indexes = generate_contract_sample(instance_count, args.sample_size)
+
+    # Extract the files with the sampled indexes along with their original indexes
+    sampled_files = [(index, file) for index, file in enumerate(files) if index in sampled_indexes]
+
+    #print(sampled_indexes, sampled_files)
+
+    for i, file_name in sampled_files:        
+        #with open(args.filename, 'r') as csv_file:
+        #with open(file_name, 'r') as file:
+        print("file_name", file_name)
+        base_file_name = os.path.basename(file_name)
+        # Remove the file extension
+        base_file_name_without_extension = os.path.splitext(base_file_name)[0]
+        block_id, target_address = base_file_name_without_extension.split('-')
+        #print("file content", file.read())
+        
+    
+        log.info('Analyzing contract %d/%d at address %s', i + 1, instance_count, target_address)
+        #print("pattth", (args.dirpath + '/' + base_file_name))
+        contract_loader = Loader.get_contract(LoaderType.BINARY, path=file_name)
+        #print(contract_loader)
+        #loader_factory = get_factory(LoaderFactoryType.JSON_RPC, address=target_address, rpc=rpc)
+        #contract_loader = loader_factory.create()
+        symbolic_analysis = SymbolicWrapper(contract_loader)    
+        report = symbolic_analysis.run_analysis()
+        pprint.pprint(report, width=1)
+        #analysis_report = SymbolicWrapper().execute(contract_loader=contract_loader, timeout=args.timeout, max_depth=args.max_depth)
+        
+        if sum(len(report_item) for report_item in report) > 0:
+            positive_instances.add(i)
+        else:
+            log.info('Nothing found for contract %d/%d at address %s', i + 1, instance_count, target_address)
+        
+        '''
+        detected_functions = [result.__dir__
+                            for report_item in report if len(report_item) > 0
+                            for result in report_item]
+        '''
+        
+        #compiler_version = row[args.version_column] if args.version_column is not None else None
+        function_hashes = contract_loader.disassembly().func_hashes if contract_loader.disassembly() else []
+
+        #print(detected_functions)
+        #benchmark_report.add_result(Result(function_hashes, target_address, i, detected_functions, compiler_version=compiler_version))
+        strategy_loader.reset_modules()
     #benchmark_report.end_time = time.strftime(TIME_FORMAT)
-    negative_instances = contract_sample - positive_instances
+    negative_instances = sampled_indexes - positive_instances
     positive_sample = set(random.sample(positive_instances, round(len(positive_instances) * args.verification_ratio)))
     negative_sample = set(random.sample(negative_instances, round(len(negative_instances) * args.verification_ratio)))
-    print("Positive sample",positive_sample)
-    print("Negative sample",negative_sample)
+    print("Positive sample", positive_sample)
+    print("sampled indexes, positive instances, Negative instances", sampled_indexes, positive_instances, negative_instances)
     #save_benchmark_state(benchmark_report, positive_sample, negative_sample)
     if args.interactive:
         #start_verification(benchmark_report, positive_sample | negative_sample)
